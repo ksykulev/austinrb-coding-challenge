c318ac22506e33e4d22be15a22b10cf547e1afd1
Nathan Hessler <xmilestegx@gmail.com>
2025-07-08 14:36:04 -0500

    remove echo on processing as it's redundant to test harness

--- runner.sh
+++ runner.sh
@@ -3,7 +3,7 @@
   # Find all main.rb files in answers subdirectories and run command on each
   find answers -name "main.rb" -path "answers/*/main.rb" | while read -r file; do
       folder=$(basename "$(dirname "$file")")
-      echo "Processing: $file"
+      # echo "Processing: $file"
 
       # Replace 'your_command' with the actual command you want to run
       ruby testharness.rb "$file"
